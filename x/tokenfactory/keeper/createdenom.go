a97702c5ddc23219e0c4cbd1d6ef730eb2e5a85d
Peter Bukva <peter.bukva@gmail.com>
2026-05-11 16:37:43 +0100

    fix: Correct initialisation of the module address

--- x/tokenfactory/keeper/createdenom.go
+++ x/tokenfactory/keeper/createdenom.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"sync"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -8,7 +10,9 @@ import (
 )
 
 var (
-	moduleAddress = authtypes.NewModuleAddress(types.ModuleName).String()
+	moduleAddress = sync.OnceValue(func() string {
+		return authtypes.NewModuleAddress(types.ModuleName).String()
+	})
 )
 
 // ConvertToBaseToken converts a fee amount in a whitelisted fee token to the base fee token amount
@@ -47,7 +51,10 @@ func (k Keeper) createDenomAfterValidation(ctx sdk.Context, adminAddr string, de
 
 		k.bankKeeper.SetDenomMetaData(ctx, denomMetaData)
 	} else {
-		creatorAddr = moduleAddress
+		// TODO(pb): Alternative solution - the commented line should be deleted once the primary solution
+		//           is proven to work.
+		//addr := k.accountKeeper.GetModuleAccount(ctx, types.ModuleName).String()
+		creatorAddr = moduleAddress()
 	}
 
 	authorityMetadata := types.DenomAuthorityMetadata{
