ba3a71e0cd4ec6be5ee825f30273c5a5f1079b66
Peter Bukva <peter.bukva@gmail.com>
2026-05-13 03:20:06 +0100

    fix: Ensure that admin check is executed

--- x/tokenfactory/types/capabilities.go
+++ x/tokenfactory/types/capabilities.go
@@ -3,18 +3,26 @@ package types
 const (
 	EnableSetMetadata   = "enable_metadata"
 	EnableForceTransfer = "enable_force_transfer"
-	// Allows to *ANY* owner of tokens (with *ANY* denomination) to burn these self-owned tokens.
-	// If disabled (not present), only admin of a denom or sudoer can execute the burn *IF* EnableBurnFrom is enabled.
+	// EnableBurnOwn Allows *ANY* owner of tokens (with *ANY* denomination) to burn these self-owned tokens.
+	// If disabled (not present), only admin of a denom can execute the burn *IF* EnableBurnFrom is enabled.
 	EnableBurnOwn = "enable_burn_own"
-	// If enabled, token owner can burn its own tokens of any denomination which is *NOT* registred in tokenfactory.
-	// This capability depends on EnableBurnOwn = so, if enabled, it has *no* effect if the EnableBurnOwn is not enabled.
+	// EnableBurnOwnUnregistered If enabled, token owner can burn its own tokens of any denomination which is *NOT*
+	// registered in tokenfactory.
+	// This EnableBurnOwnUnregistered has effect only if the EnableBurnOwn is enabled as well.
 	EnableBurnOwnUnregistered = "enable_burn_unregistered"
 	EnableBurnFrom            = "enable_burn_from"
-	// Allows addresses registered as sudo admins imn genesis store to mint tokens of *ANY* denominations.
-	// NOTE: with SudoMint enabled, the sudo admin can mint `any` token, not just tokenfactory tokens.
-	// This is intended behavior as requested by other teams, rather than having its own module with very minor logic.
-	// If you do not wish for this behavior, then either do NOT enable this capability, or implement your own logic.
+	// EnableSudoMint is a High level enabler for minting and burning unbound denominations(= any denominations which
+	// do *NOT* conform to the tokenfactory denomination format `factory/<CREATOR_ADDRESS>/<SUB_DENOM>`).
+	// In order to enable minting of unbound denominations, such denominations *MUST* be registered in tokenfactory and
+	// EnableSudoMint must be enabled.
+	// By design, it *NOT* possible to register unbound denomination via tokenfactory MsgCreateDenom message, instead
+	// it *MUST* be registered either in `genesis.json` file, or during chain software upgrade using the
+	// `UnboundDenomCreator` interface.
+	// In order to enable burning of unbound denominations, the EnableSudoMint must be enabled. It is possible to burn
+	// unbound denominations which are not registered in token factory, however both - the EnableBurnOwn and
+	// EnableBurnOwnUnregistered must be enabled.
 	EnableSudoMint = "enable_admin_sudo_mint"
+
 	// EnableCommunityPoolFeeFunding sends tokens to the community pool when a new fee is charged (if one is set in params).
 	// This is useful for ICS chains, or networks who wish to just have the fee tokens burned (not gas fees, just the extra on top).
 	EnableCommunityPoolFeeFunding = "enable_community_pool_fee_funding"
